e21bf6ca72bf598d50e93d7249f09deafd462f51
Zainhom16 <131176554+Zainhom16@users.noreply.github.com>
2024-04-29 00:17:22 +0300

    Add drizzle configurations and other dependencies

---
+++ drizzle.config.js
@@ -0,0 +1,8 @@
+export default {
+  schema: "./utils/schema.jsx",
+  driver: "pg",
+  dbCredentials: {
+    connectionString:
+      "postgresql://neondb_owner:m1iJTwnHpvI6@ep-gentle-dawn-a2qh3ogp.eu-central-1.aws.neon.tech/Spendify?sslmode=require",
+  },
+};
