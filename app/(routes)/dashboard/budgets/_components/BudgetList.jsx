7486293def9626655281872d5c639471d133b505
Rana Amr <106626014+ranaamr22@users.noreply.github.com>
2024-04-29 23:09:25 +0300

    feat: add Budget List Functionality

--- app/(routes)/dashboard/budgets/_components/BudgetList.jsx
+++ app/(routes)/dashboard/budgets/_components/BudgetList.jsx
@@ -1,1 +1,7 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import CreateBudget from "./CreateBudget";
+import { db } from "@/utils/dbConfix";
+import { desc, getTableColumns, sql } from "drizzle-orm";
+import { Budgets, Expenses } from "@/utils/schema";
 
