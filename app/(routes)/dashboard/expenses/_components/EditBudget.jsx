aa0471fbaedd261a96caecb22161e87ae8eca58f
fkamalo <167641692+fkamalo@users.noreply.github.com>
2024-04-29 16:27:02 +0300

    Add edit budget functionality

--- app/(routes)/dashboard/expenses/_components/EditBudget.jsx
+++ app/(routes)/dashboard/expenses/_components/EditBudget.jsx
@@ -1,1 +1,120 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { PenBox } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter,
+  DialogClose,
+} from "@/components/ui/dialog";
+import EmojiPicker from "emoji-picker-react";
+import { useUser } from "@clerk/nextjs";
+import { Input } from "@/components/ui/input";
+import { db } from "@/utils/dbConfix";
+import { Budgets } from "@/utils/schema";
+import { toast } from "sonner";
+import { eq } from "drizzle-orm";
 
+function EditBudget({ budgetInfo, refreshData }) {
+  let [emojiIcon, setEmojiIcon] = useState(budgetInfo?.icon);
+  let [openEmojiPicker, setOpenEmojiPicker] = useState(false);
+
+  let [name, setName] = useState();
+  let [amount, setAmount] = useState();
+  const { user } = useUser();
+
+  useEffect(() => {
+    if (budgetInfo) {
+      setEmojiIcon = budgetInfo?.icon;
+      setName = budgetInfo?.name;
+      setAmount = budgetInfo?.amount;
+    }
+  }, [budgetInfo]);
+  const onUpdateBudget = async () => {
+    const result = await db
+      .update(Budgets)
+      .set({
+        name: name,
+        amount: amount,
+        icon: emojiIcon,
+      })
+      .where(eq(Budgets.id, budgetInfo.id))
+      .returning();
+    if (result) {
+      refreshData();
+      toast("Budget Updated");
+    }
+  };
+  return (
+    <div>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button className="flex gap-2  bg-primaryColor hover:bg-darkPrimary text-white">
+            <PenBox />
+            Edit
+          </Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Update Budget</DialogTitle>
+            <DialogDescription>
+              <div className="mt-5">
+                <Button
+                  className="text-lg"
+                  variant="outline"
+                  onClick={() => setOpenEmojiPicker(!openEmojiPicker)}
+                >
+                  {emojiIcon}
+                </Button>
+                <div className="absolute z-20">
+                  <EmojiPicker
+                    open={openEmojiPicker}
+                    onEmojiClick={(e) => {
+                      setEmojiIcon(e.emoji);
+                      setOpenEmojiPicker(false);
+                    }}
+                  />
+                </div>
+                <div className="mt-2">
+                  <h2 className="text-black font-medium my-1">Budget Name</h2>
+                  <Input
+                    defaultValue={budgetInfo?.name}
+                    placeholder="e.g. Car Maintenance"
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+                <div className="mt-2">
+                  <h2 className="text-black font-medium my-1">Budget Amount</h2>
+                  <Input
+                    defaultValue={budgetInfo?.amount}
+                    type="number"
+                    placeholder="e.g. $ 200"
+                    onChange={(e) => setAmount(e.target.value)}
+                  />
+                </div>
+              </div>
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter className="sm:justify-start">
+            <DialogClose asChild>
+              <Button
+                disabled={!(name && amount)}
+                onClick={() => onUpdateBudget()}
+                className="mt-5 w-full bg-primaryColor hover:bg-darkPrimary text-white"
+              >
+                Update Budget
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
+
+export default EditBudget;
