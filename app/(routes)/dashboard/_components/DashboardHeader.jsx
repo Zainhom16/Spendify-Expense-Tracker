41b37e4307c65c2deb839a3f8f3d2db4c32d7fc2
Rana Amr <106626014+ranaamr22@users.noreply.github.com>
2024-04-29 23:06:56 +0300

    feat: add Dashboard Header Functionality

--- app/(routes)/dashboard/_components/DashboardHeader.jsx
+++ app/(routes)/dashboard/_components/DashboardHeader.jsx
@@ -1,1 +1,23 @@
+import { UserButton } from "@clerk/nextjs";
+import { Input } from "@/components/ui/input";
 
+import React from "react";
+
+function DashboardHeader() {
+  return (
+    <div className="p-5 shadow-sm border-b flex justify-between items-center ">
+      <div className=" md:w-80 lg:w-96">
+        <Input
+          type="text"
+          class="w-full py-2 px-4 rounded-lg border border-gray-300 focus:outline-none focus:border-blue-600"
+          placeholder="🔍  Search"
+        />
+      </div>
+      <div className="size">
+        <UserButton showName="true" />
+      </div>
+    </div>
+  );
+}
+
+export default DashboardHeader;
