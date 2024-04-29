b441a5eaf8937812a01ea30d8e531c90a52d87a8
Noha-A-Kh <125829152+Noha-A-Kh@users.noreply.github.com>
2024-04-29 23:36:48 +0300

    added header

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,1 +1,18 @@
+import React from "react";
+import Image from "next/image";
 
+function Header() {
+  return (
+    <div className="flex justify-center items-center gap-2 mx-auto mt-8 border-b border-gray-300 shadow-sm pb-4">
+      <Image
+        src={"/logo.png"}
+        alt="Website Logo"
+        width={300}
+        height={300}
+        className="mb-1"
+      />
+    </div>
+  );
+}
+
+export default Header;
